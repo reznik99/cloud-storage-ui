41b7537327bed497df512b63218008d6ebb469da
Francesco <goras.francesco@gmail.com>
2024-09-22 17:02:14 +1200

    Improved settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,21 +1,27 @@
 import { AccountCircle, ArrowBack, Edit, Password } from '@mui/icons-material'
-import { Box, Chip, Container, FormControl, FormLabel, IconButton, InputAdornment, LinearProgress, Paper, Stack, TextField, Typography } from '@mui/material'
+import { Box, Chip, Container, Divider, FormControl, FormControlLabel, FormLabel, IconButton, InputAdornment, LinearProgress, Paper, Stack, Switch, TextField, Typography, useColorScheme } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { RootState } from '../store/store'
+import { calculateSizePercentageUsed, calculateSizeUsed, sizePercentageToColor } from '../utilities/utils'
 
 function Settings() {
     const navigate = useNavigate()
     const data = useSelector((state: RootState) => state.user)
-    console.log(data)
+    const { mode, setMode } = useColorScheme()
+
+    const sizeUsed = calculateSizeUsed(data.files)
+    const sizeUsedPercentage = calculateSizePercentageUsed(sizeUsed, 1000)
     return (
-        <Container component={Paper}>
+        <Container component={Paper} sx={{ padding: 5 }}>
             <IconButton onClick={() => navigate(-1)}><ArrowBack /></IconButton>
 
             <Stack direction='column'
                 spacing={2}
-                sx={{ padding: 10 }}>
+                sx={{ padding: 2 }}>
+
+                <Divider><Typography variant='h5'>Account Details</Typography></Divider>
                 <FormControl>
                     <FormLabel>Email Address</FormLabel>
                     <TextField fullWidth
@@ -64,18 +70,36 @@ function Settings() {
                 </FormControl>
 
                 <FormControl>
-                    <FormLabel>Account active since</FormLabel>
-                    <Chip content={data.createdAt} />
+                    <FormLabel>Account active since <Chip label={data.createdAt} /></FormLabel>
                 </FormControl>
+            </Stack>
 
+            <Stack direction='column'
+                spacing={2}
+                sx={{ padding: 2 }}>
+                <Divider><Typography variant='h5'>File Details</Typography></Divider>
                 <FormControl>
-                    <FormLabel>Storage used</FormLabel>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Typography variant="body2">{data.files?.length} files at 512.4 MB/1000 MB</Typography>
-                        <LinearProgress variant='determinate' value={45} />
-                    </Box>
+                    <FormLabel>Files stored: <Chip label={data.files?.length || 0} /></FormLabel>
                 </FormControl>
+                <FormControl>
+                    <FormLabel>Storage used: <Chip label={`${sizeUsed} MiB/1,000 MiB (${sizeUsedPercentage}%)`} /></FormLabel>
+                </FormControl>
+                <LinearProgress variant="determinate" color={sizePercentageToColor(sizeUsedPercentage)} value={sizeUsedPercentage} />
+            </Stack>
 
+            <Stack direction='column'
+                spacing={2}
+                sx={{ padding: 2 }}>
+                <Divider><Typography variant='h5'>Preferences</Typography></Divider>
+                <Box>
+                    <FormLabel>Color Theme</FormLabel>
+                    <FormControlLabel checked={mode === 'dark'}
+                        onChange={() => setMode(mode === 'light' ? 'dark' : 'light')}
+                        control={<Switch color="primary" />}
+                        label={mode}
+                        labelPlacement="start"
+                    />
+                </Box>
             </Stack>
         </Container>
     )
