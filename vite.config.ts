0e49dabdbded5852ac8088184d92abb49123488d
Francesco <goras.francesco@gmail.com>
2025-02-01 16:10:09 +1300

    Added SRI (Subresource Integrity) and CSP (Content Security Policy) plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import csp from "vite-plugin-csp-guard";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    csp({
+      algorithm: "sha384",
+      dev: { run: true },
+      build: { sri: true },
+      policy: {
+        "script-src": ["'self'"],
+        "style-src-elem": ["'self'", "'unsafe-inline'"],
+        "font-src": ["'self'"],
+      }
+    })
+  ],
 })
